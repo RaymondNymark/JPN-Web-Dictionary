6ed0a3574cee4badda492e4eed180ee5fbe4c03b
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-19 13:26:07 +0100

    Create new ApiManager class to initialize a static Http client in order to save on resources and remove unused class.

---
+++ Dictionary.Core/Services/APIManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Dictionary.Core.Services
+{
+    public static class ApiManager
+    {
+        // Static so it only opens once to save performance. 
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        public static void InitializeAPIClient()
+        {
+            ApiClient = new HttpClient();
+            // Sets base address as the entire API so requests won't have to
+            // have the full URL in them, instead only the actual search term.
+            ApiClient.BaseAddress = new Uri("https://jisho.org/api/v1/search/words?keyword=");
+
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
