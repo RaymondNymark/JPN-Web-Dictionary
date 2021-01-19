38bdb4d8732db10e7ed70cbe92e280514dc293ff
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-19 13:27:50 +0100

    Create DictionarySearchService.cs

---
+++ Dictionary.Core/Services/DictionarySearchService.cs
@@ -0,0 +1,19 @@
+﻿using Dictionary.Core.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary.Core.Services
+{
+    public class DictionarySearchService
+    {
+        public DictionarySearchService()
+        {
+            // Initializes the API Client.  TODO : Move this to autostart.
+            ApiManager.InitializeAPIClient();
+        }
+    }
+}
