2fc9e52cb53884f7ca7b0d1c7a79f155c049684a
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-20 10:13:01 +0100

    Remove all default included componets from the application and remove references to them

--- Dictionary.Blazor/Startup.cs
+++ Dictionary.Blazor/Startup.cs
@@ -1,4 +1,5 @@
 using Dictionary.Blazor.Data;
+using Dictionary.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +29,7 @@ namespace Dictionary.Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<IDictionarySearchService, DictionarySearchService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
