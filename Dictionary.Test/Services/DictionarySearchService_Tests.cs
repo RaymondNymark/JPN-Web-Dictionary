ccd718df83553cbf5b1d6d577f4656a320ebce33
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-20 08:05:33 +0100

    Swap out entire Xunit testing library and replace with default MS Unit testing in order to be able to unit test against .NET standard class library
    The core library is .net standard and thus makes unit testing a bit tricky.  Xunit doesn't support it what-so-ever, so replacing it for MS unit testing is a fair solution as it works fine.

---
+++ Dictionary.Test/Services/DictionarySearchService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Dictionary.Core.Services;
+
+namespace Dictionary.Tests.Services
+{
+    [TestClass]
+    public class DictionarySearchService_Tests
+    {
+        [TestMethod]
+        public void SearchDictionaryAsync_ValidCallShouldReturnValidData()
+        {
+            Assert.AreEqual(true, false);
+        }
+
+        [TestMethod]
+        public void ShouldPass()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
