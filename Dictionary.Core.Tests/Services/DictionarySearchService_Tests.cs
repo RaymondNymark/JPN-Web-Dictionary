553e5f31fa3913f3055ff30da4399a627a564cdb
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-11-03 09:39:13 +0100

    Replace MS.test library in favor of Xunit testing

---
+++ Dictionary.Core.Tests/Services/DictionarySearchService_Tests.cs
@@ -0,0 +1,18 @@
+﻿using Dictionary.Core.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dictionary.Core.Tests.Services
+{
+    public class DictionarySearchService_Tests
+    {
+        [Fact]
+        public async Task SearchDictionaryAsync_ValidCallShouldReturnAsync()
+        {
+            var dictionarySearch = new DictionarySearchService();
+            var returnedCollection = await dictionarySearch.SearchDictionaryAsync("omoiyari");
+            var actual = returnedCollection.Count > 1;
+            Assert.True(actual);
+        }
+    }
+}
